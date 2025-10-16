532abc572c89a5ded4305fbbe9dba0b90c54dd86
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-10-16 07:27:49 -0300

    .

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-function criaCartao( categoria, pergunta, resposta) {}
+function criaCartao( categoria, pergunta, resposta) {
+    let container = document.getElementById('container');
+    let cartao = document.getElementById('article');
+    cartao.className = 'cartao';
+}
