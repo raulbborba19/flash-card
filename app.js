f889b8c4400446b810774f0210baa1be9798226d
raulbborba19 <raul.borba@escola.pr.gov.br>
2025-10-17 07:56:46 -0300

    .

--- app.js
+++ app.js
@@ -3,3 +3,14 @@ function criaCartao( categoria, pergunta, resposta) {
     let cartao = document.getElementById('article');
     cartao.className = 'cartao';
 }
+
+cartao.innerHTML =
+    <div class="cartao_conteudo">
+        <h3>Programação</h3>
+        <div class="cartao_conteudo_pergunta">
+            <p>O que é JavaScript?</p>
+        </div>
+        <div class="cartao_conteudo_resposta">
+            <p>O JavaScript é uma linguagem de Programação</p>
+        </div>
+    </div>
